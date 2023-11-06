2bb8ca49985554af2d7b23552cb4dcf017cdf21c
Alexander Reuter <alexanderreuter713@gmail.com>
2023-11-06 12:29:23 +0100

    basal svg

---
+++ script.js
@@ -0,0 +1,7 @@
+// Width og height til SVG
+const w = 1000;
+const h = 500;
+//Padding for at søjlerne ikke skal sidde helt op af hinanden
+const padding = 10;
+//Padding til akserne for at man kan se det hele
+const axisPadding = 70;
