b94285aece585e49fd3400fc1986f832231ee79b
RasmusOlesen00 <eaa23rao@students.eaaa.dk>
2023-11-07 14:56:23 +0100

    akser og skalering

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 // Width og height til SVG
 const w = 1000;
 const h = 500;
-//Padding for at søjlerne ikke skal sidde helt op af hinanden
+
+
+// Padding for at søjlerne ikke skal sidde helt op af hinanden
 const padding = 10;
-//Padding til akserne for at man kan se det hele
+
+
+// Padding til akserne for at man kan se det hele
 const axisPadding = 70;
+
 
 // Hent data fra albums.json
 fetch('albums.json')
@@ -21,9 +26,15 @@ fetch('albums.json')
             return album.albumName;
         });
 
-//Denne kode opretter et SVG-element og tilføjer det til HTML-kroppen med en angivet bredde og højde.
-const svg = d3
-  .select("body")
-  .append("svg")
-  .attr("width", w)
-  .attr("height", h);
+
+// Denne kode opretter et SVG-element og tilføjer det til HTML-kroppen med en angivet bredde og højde.
+    const svg = d3 .select("body").append("svg").attr("width", w).attr("height", h);
+
+
+// Der defineres en Skaleringsfunktion, som værdisættes senere
+let yScale = null;
+let xScale = null;
+
+// Der defineres en variabel for Akserne, som værdisættes senere 
+let xAxis = null;
+let yAxis = null;
